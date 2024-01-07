3db64b07feb7dc72d50d6e6e6dd6f335904d9f35
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-08 01:29:54 +0200

    1-block-scoped.js

--- 0x00-ES6_basic/0-constants.test.js
+++
@@ -1,9 +0,0 @@
-import { taskNext, taskFirst } from "./0-constants";
-
-test('test 1', ()=> {
-	expect(taskFirst()).toBe('I prefer const when I can.');
-});
-
-test('test 2', ()=> {
-	expect(taskNext()).toBe('But sometimes let is okay');
-});
---
+++ 0x00-ES6_basic/1-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable jest/require-hook */
+// eslint-disable-next-line import/extensions
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
