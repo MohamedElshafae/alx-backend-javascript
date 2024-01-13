65c05c2995b70fb650cb0f42788df8d294ca2964
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-13 05:16:27 +0200

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+export default function getFullResponseFromAPI(success) {
+  return new Promise((res, rej) => {
+    if (success) {
+      res({ status: 200, body: 'Success' });
+    } else {
+      rej(Error('The fake API is not working currently'));
+    }
+  });
+}
