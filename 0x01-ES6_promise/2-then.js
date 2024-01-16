3ade8ce48beb74a7bb193f8f46af68148a613a4e
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-16 02:21:35 +0200

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then((res) => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
+      return { body: 'success', status: 200 };
     })
-    .catch((err) => Error);
+    .catch((err) => new Error());
 }
