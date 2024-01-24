dc2527a49f154b56b9f5198376d22f5600b49b24
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-24 07:35:52 +0200

    fix Error

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,7 +1,9 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = name;
-    this._code = code;
+    if (typeof code === 'string' && typeof name === 'string') {
+      this._code = code;
+      this._name = name;
+    }
   }
 
   get [Symbol.toStringTag]() {
@@ -9,26 +11,6 @@ export default class Airport {
   }
 
   toString() {
-    return `object [${this.code}]`;
-  }
-
-  set code(code) {
-    if (typeof code === 'string') {
-      this._code = code;
-    }
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    }
-  }
-
-  get name() {
-    return this._name;
+    return `object [${this._code}]`;
   }
 }
