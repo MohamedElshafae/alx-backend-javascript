430be0410c33c7008fe9b9f29b93e1cfec4cb1f1
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-24 06:57:30 +0200

    fix Error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,9 +2,6 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-  }
-
-  evacuationWarningMessage() {
     if (!this.evacuationWarningMessage) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage',
