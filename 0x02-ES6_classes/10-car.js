b489d42836f16f3849db111a3a4e23e539f62121
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-24 08:27:13 +0200

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (
+      typeof brand === 'string'
+      && typeof motor === 'string'
+      && typeof color === 'string'
+    ) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
