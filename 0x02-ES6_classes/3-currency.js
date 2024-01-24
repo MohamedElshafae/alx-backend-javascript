a27d3df5ab72d61d86242ce8ddf6ea19576bc008
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-24 05:26:52 +0200

    fix error

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,4 +7,24 @@ export default class Currency {
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
 }
