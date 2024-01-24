1ceafd815fe9c490bfa1fcd569acd00ceb39c9e7
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-24 06:50:12 +0200

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+/* eslint-disable class-methods-use-this */
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value === 'number') {
+      this._sqft = value;
+    }
+  }
+}
