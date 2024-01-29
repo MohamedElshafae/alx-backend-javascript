2e06dcc6d3f7dc2f7949ae50cb3d5250b392299e
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-29 11:18:02 +0200

    tring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (startString === '') return '';
 
   const list = Array.from(set)
-    .filter((element) => element.startsWith(startString))
+    .filter((element) => typeof element === 'string' && element.startsWith(startString))
     .map((element) => element.slice(startString.length));
 
   return list.join('-');
