869c32e78d90da0df6216994edcaa1ba7c8bfeeb
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-29 11:05:32 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,9 @@
 export default function cleanSet(set, startString) {
   if (startString === '') return '';
-  const list = [];
 
-  set.forEach((element) => {
-    if (element.startsWith(startString)) {
-      list.push(element.slice(startString.length));
-    }
-  });
+  const list = Array.from(set)
+    .filter((element) => element.startsWith(startString))
+    .map((element) => element.slice(startString.length));
 
   return list.join('-');
 }
