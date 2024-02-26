194469261961b8c100d2b522ea88cb34bf453051
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-26 23:30:10 +0200

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) process.stdout.write(`Your name is: ${data}\n`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
