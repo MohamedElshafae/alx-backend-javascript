612af7db608212e42451b0834dd3da1a7a1ee944
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-03-01 05:43:42 +0200

    readfile with sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,15 +7,17 @@ const countStudents = (path) => {
     const lines = data.split('\n').filter((line) => line.trim() !== '');
     lines.shift();
     const dictionary = {};
+    let count = 0;
     lines.forEach((line) => {
       const temp = line.trim().split(',');
+      count++;
       const firstName = temp[0];
       const field = temp[3];
       if (dictionary[field]) {
         dictionary[field].push(firstName);
       } else dictionary[field] = [firstName];
     });
-
+    console.log(`Number of students: ${count}`);
     for (const key in dictionary) {
       console.log(`Number of students in ${key}: ${dictionary[key].length}. List: ${dictionary[key].join(', ')}`);
     }
