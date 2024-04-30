0944914512a6f353a0e16cae2db8fb4d0bc0da17
MohamedElshafae <mohamedelshafae888@gmail.com>
2024-05-01 02:50:38 +0300

    5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,46 @@
+/* eslint-disable guard-for-in */
+const http = require('http');
+const fs = require('fs').promises;
+
+const port = 1245;
+const path = process.argv[2];
+
+const countStudents = async (path) => {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    lines.shift();
+    const dictionary = {};
+    let count = 0;
+    lines.forEach((line) => {
+      const temp = line.trim().split(',');
+      count += 1;
+      const firstName = temp[0];
+      const field = temp[3];
+      if (dictionary[field]) {
+        dictionary[field].push(firstName);
+      } else {
+        dictionary[field] = [firstName];
+      }
+    });
+    let output = `Number of students: ${count}\n`;
+    for (const key in dictionary) {
+      output += `Number of students in ${key}: ${dictionary[key].length}. List: ${dictionary[key].join(', ')}\n`;
+    }
+    return output;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') res.end('Hello Holberton School!');
+  else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    const studentList = await countStudents(path);
+    res.end(studentList);
+  }
+});
+
+app.listen(port);
+module.exports = app;
